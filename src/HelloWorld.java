4736295b531ec28de1c14016c3c3a49d53c70071
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-09-28 17:04:23 -0500

    This commit finishes the exercises for Java Syntax

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,36 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, ");
-        System.out.print("World!");
+//        System.out.print("Hello, ");
+//        System.out.print("World!");
+//        int myFavoriteNumber = 6;
+//        System.out.println(myFavoriteNumber);
+//        String myString = 3.14159;
+//        System.out.println(myString);
+//        char stringChar = myString;
+//        System.out.println(stringChar);
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        char class =
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        int x = 4;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        System.out.println(y * x);
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
     }
+
 }
