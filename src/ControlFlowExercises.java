a7f378002aa729cf6426f642f3a9e7d4b22a01a8
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-09-30 23:42:10 -0500

    This commit finishes 2nd exercise group

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,27 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        // While Loop
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.printf("%s ", i);
+//            i++;
+//        }
+        // Do While Loop
+//        int i = 0;
+//        do{
+//            System.out.printf("\n %s", i);
+//            i+=2;
+//        }while(i<101);
+//        int i = 100;
+//        do{
+//            System.out.printf("%s \n", i);
+//            i-=5;
+//        }while(i >= -10);
+        long i = 2;
+        do{
+            System.out.printf("%s\n", i);
+            i*=i;
+        }while(i <= 1000000);
+
+    }
+}
