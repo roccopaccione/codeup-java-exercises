c3cd2d363c149183d87682a2bbae9f7f6307ed69
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-10-02 10:31:13 -0500

    feat: String exercises of the string lecture

---
+++ src/StringExercise.java
@@ -0,0 +1,14 @@
+public class StringExercise {
+
+    public static void main (String[] args) {
+
+        String anotherBrickInTheWall = "We don't need no education \nWe don't need no thought control";
+        System.out.println(anotherBrickInTheWall);
+
+        String check = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(check);
+
+        String windows = "In Windows, the main drive is usually C:\\";
+        System.out.println(windows);
+    }
+}
