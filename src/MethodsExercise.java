d41c96ac2052d8ae641eeb5a41bdef603ad3615d
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-10-02 16:58:41 -0500

    This commit adds the methods exercises

---
+++ src/MethodsExercise.java
@@ -0,0 +1,24 @@
+public class MethodsExercise {
+    public static void main (String[] args) {
+        addition(5, 10);
+
+        subtraction(25,32);
+
+    }
+    public static int addition (int one, int two) {
+        return (one + two);
+
+    }
+
+    public static int subtraction (int one, int two) {
+        return (one - two);
+    }
+
+    public static int multiplication (int one, int two) {
+        return (one * two);
+    }
+
+    public static int division (int one, int two) {
+        return (one / two);
+    }
+}
