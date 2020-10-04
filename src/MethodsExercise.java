299e4e05404eb72dd4e485aecc41ee9b84e6cbf8
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-10-03 23:54:18 -0500

    This Commit adds correct method overload of arithmetic operations

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -1,27 +1,34 @@
 public class MethodsExercise {
     public static void main (String[] args) {
-        addInt(5, 10);
 
-        minusInt(65, 22);
+        System.out.println(addInt(5, 10));
 
-        increaseBy(42, 20);
+        System.out.println(subtractNum(65, 22));
 
-        division(225, 5);
+        System.out.println(multiplyNum(42, 20));
+
+        System.out.println(division(225, 5));
+
+        System.out.println(modNumBy(30, 3));
 
     }
     public static int addInt (int one, int two) {
         return one + two;
     }
 
-    public static int minusInt (int first, int second) {
+    public static int subtractNum (int first, int second) {
         return first - second;
     }
 
-    public static int increaseBy (int one, int two) {
+    public static int multiplyNum (int one, int two) {
         return one * two;
     }
 
     public static int division (int one, int two) {
-        return (one / two);
+        return one / two;
+    }
+
+    public static int modNumBy (int one, int two) {
+        return one % two;
     }
 }
