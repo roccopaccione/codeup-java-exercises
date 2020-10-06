6a2208ecd59f607a907222584295de1820eba045
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-10-06 12:22:46 -0500

    This commit adds finished files for dish_tools.java

---
+++ src/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    private String name;
+
+    public String getName(){
+//TODO: return the person's name
+        return this.name;
+    }
+
+    public void setName(String name){
+//TODO: change the name property to the passed value
+        this.name = name;
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.printf("Hello %s!", this.name);
+    }
+
+    public static void main(String[] args) {
+        Person rocco =new Person();
+        rocco.name = "Rocco Paccione";
+    }
+
+}
