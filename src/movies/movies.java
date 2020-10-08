4fb0173a0d6f4e3adbf6ad12e5c50a9169dc3657
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-10-08 11:16:35 -0500

    finished arrays exercise file

---
+++ src/movies/movies.java
@@ -0,0 +1,20 @@
+public class movies {
+
+    public static void Movie(){
+        Movie[] movies;
+        private movies[] getName(String[] name){
+
+        }
+
+        private String[] category(){
+
+        }
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
+
+}
