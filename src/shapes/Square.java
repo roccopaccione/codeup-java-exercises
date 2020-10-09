c9e7aaf4d49a3835cc0221808c282f6099172c09
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-10-08 23:33:05 -0500

    This commit adds Rectangle blueprint and starts square blueprint

---
+++ src/shapes/Square.java
@@ -0,0 +1,8 @@
+package shapes;
+
+public class Square extends Rectangle {
+
+    public void square(double side){
+
+    }
+}
--- src/util/Input.java
+++ src/util/Input.java
@@ -3,7 +3,7 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-    private Scanner scanner;
+    private static Scanner scanner;
 
     public Input(){
         this.scanner = new Scanner(System.in);
