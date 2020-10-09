3693e507798c8dbdcba7ad0a14fcba21b745c493
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-10-09 00:05:27 -0500

    This Commit adds Rectangle and Square exercises

---
+++ src/shapes/ShapeTest.java
@@ -0,0 +1,15 @@
+package shapes;
+
+public class ShapeTest {
+
+    public static void main(String[] args){
+
+
+//        Rectangle box1(){
+//            rectangle.length = 5;
+//
+//
+//        }
+
+    }
+}
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -3,6 +3,7 @@ package shapes;
 public class Square extends Rectangle {
 
     public void square(double side){
-
+        super.length = side;
+        super.width = side;
     }
 }
