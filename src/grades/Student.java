7317800ce4796045b4e9a3c5b0bf5ad4728065fb
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-10-12 23:25:01 -0500

    This commit adds the first Collections exercise

---
+++ src/grades/Student.java
@@ -0,0 +1,14 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student{
+    private String name;
+
+    ArrayList<Integer> grades = new ArrayList<>();
+
+    public Student(){
+
+    }
+
+}
