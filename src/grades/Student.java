71e4a16f262ac312c7516fb8882e4dc9148d157f
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-10-13 19:44:58 -0500

    feat: Boilerplate for first Collections exercise

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -4,10 +4,26 @@ import java.util.ArrayList;
 
 public class Student{
     private String name;
+    private final ArrayList<Integer> grades = new ArrayList<>();
 
-    ArrayList<Integer> grades = new ArrayList<>();
+    public Student(String name, Integer grade){
+        this.name = name;
+        ArrayList<Integer> grades = new ArrayList<>();
 
-    public Student(){
+//        // returns the student's name
+//        public String getName(){
+//            return name;
+//        }
+//// adds the given grade to the grades property
+//        public void addGrade(int grade);
+//// returns the average of the students grades
+//        public double getGradeAverage();
+
+
+    }
+
+    public static void main(String[] args){
+        String newStudent = new Student("Rocco", 75);
 
     }
 
